007372e6a187360de4aa11a3f02fb98125dfe02d
miltonbolonha <miltonbolonha@gmail.com>
2021-05-02 22:17:42 -0300

    v0.1.3 fix error msg

--- data/index.js
+++ data/index.js
@@ -28,27 +28,27 @@ const newCapitulo = subAgent => {
 		return null
 	}
 
-	fs.readdir('./data/oneBigString', (err, files) => {
-		if (err) {
-			console.error(err)
-			return
-		}
+	// fs.readdir('./data/oneBigString', (err, files) => {
+	// 	if (err) {
+	// 		console.error(err)
+	// 		return
+	// 	}
 
-		files.map(theFile => {
-			// theFile.split(/(\w)(\-)(\d)(\.)/)
-			// console.log(theFile.split(/(\w)(\-)(\d)(\.)/))
-			const digitsCount = files.length.toString().length
-			const numeroUltra = theFile.slice(9, -3).padStart(digitsCount, '0')
-			const digitPattern = subAgent.toString().padStart(digitsCount, '0')
-			if (numeroUltra === digitPattern) {
-				// console.log('Tem sim, o capítulo: ', numeroUltra)
-				// console.log('capitulo-' + digitPattern + '.js')
-			}
-			// console.log(digitPattern)
+	// 	files.map(theFile => {
+	// 		// theFile.split(/(\w)(\-)(\d)(\.)/)
+	// 		// console.log(theFile.split(/(\w)(\-)(\d)(\.)/))
+	// 		const digitsCount = files.length.toString().length
+	// 		const numeroUltra = theFile.slice(9, -3).padStart(digitsCount, '0')
+	// 		const digitPattern = subAgent.toString().padStart(digitsCount, '0')
+	// 		if (numeroUltra === digitPattern) {
+	// 			// console.log('Tem sim, o capítulo: ', numeroUltra)
+	// 			// console.log('capitulo-' + digitPattern + '.js')
+	// 		}
+	// 		// console.log(digitPattern)
 
-			// numeroUltra === digitPattern ? console.log(digitPattern) : null
-		})
-	})
+	// 		// numeroUltra === digitPattern ? console.log(digitPattern) : null
+	// 	})
+	// })
 }
 newCapitulo(13)
 
