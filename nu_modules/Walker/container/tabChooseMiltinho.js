1a093de55de3786c22169d9821b89cc1dc8d1127
Milton Bolonha <contato@miltonbolonha.com.br>
2021-05-04 21:18:12 -0300

    resolving path bugfix

--- nu_modules/Walker/container/tabChooseMiltinho.js
+++ nu_modules/Walker/container/tabChooseMiltinho.js
@@ -2,10 +2,12 @@ const dotenv = require('dotenv')
 const fs = require('fs')
 const term = require('terminal-kit').terminal
 var path = require('path');
+const appRoot = require('app-root-path');
 
 const clearConsole = require('clear-any-console')
 const chooseMiltinho = () => {
-	var fullpath = path.resolve('./.env.staging')
+	// return console.log(appRoot)
+	var fullpath = path.resolve(`${appRoot}/.env.staging`)
 
 	const file = dotenv.parse(fs.readFileSync(fullpath))
 	const dataInsert = `HERO_NAME=${file.HERO_NAME}
@@ -15,10 +17,11 @@ HERO_TERMN=${file.HERO_TERMN}
 HERO_TERMN_AO=${file.HERO_TERMN_AO}
 HERO_INIT_NAME=${file.HERO_INIT_NAME}`
 
-	const filename = './.env'
+	const filename = path.resolve(`${appRoot}/.env`)
+	console.log(path.resolve(filename))
 	//  appendFileSync
 	fs.writeFileSync(filename, dataInsert, 'utf-8')
-	const fileFinal = dotenv.parse(fs.readFileSync('./.env'))
+	const fileFinal = dotenv.parse(fs.readFileSync(filename))
 
 	const heroName = fileFinal.HERO_NAME
 	const heroTermn = fileFinal.HERO_TERMN
